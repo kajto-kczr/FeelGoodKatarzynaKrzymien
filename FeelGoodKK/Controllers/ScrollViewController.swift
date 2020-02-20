cba6ff5b72472f8970dc9adc4713a06850aba369
Kajetan Kuczorski <kajto.kuczorski@outlook.com>
2020-02-20 17:54:19 +0100

    2nd

---
+++ FeelGoodKK/Controllers/ScrollViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ScrollViewController.swift
+//  FeelGoodKK
+//
+//  Created by Kajetan Kuczorski on 20.02.20.
+//  Copyright © 2020 Kajetan Kuczorski. All rights reserved.
+//
+
+import UIKit
+
+class ScrollViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
